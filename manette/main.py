22c8549bb9ecceb1eaa9b3ba6fa32c404339fd0a
mleo <leo.mace@ynov.com>
2023-10-04 14:52:59 +0200

    Implémantation tous les boutons principaux

--- manette/main.py
+++ manette/main.py
@@ -1,45 +1,89 @@
-import ubinascii
 import time
 import espnow
 from machine import Pin, ADC
 import network
 
-CURRENT_MAC = "" #@"\xd8\xea\xd5\x10
+# @"\xd8\xea\xd5\x10
+
+PIN_BTN_ACCESSOIRE = Pin(16, Pin.IN, Pin.PULL_UP)
+
+
+PIN_INTERRUPT_GIRO = Pin(19, Pin.OUT)
+PIN_INTERRUPT_GIRO2 = Pin(15, Pin.IN)
 
 
 JOYSTICK_X = ADC(Pin(35))
 JOYSTICK_Y = ADC(Pin(34))
 
-PIN_BTN_ACCESSOIRE = Pin(16, Pin.IN, Pin.PULL_UP)
-
 JOYSTICK_X.atten(ADC.ATTN_11DB)
 JOYSTICK_Y.atten(ADC.ATTN_11DB)
 
-wlan_sta = network.WLAN(network.STA_IF)
-wlan_sta.active(True)
-wlan_mac = wlan_sta.config('mac')
-CURRENT_MAC = ubinascii.hexlify(wlan_mac).decode()
-print(wlan_mac)
 
-e = espnow.ESPNow()
-e.active(True)
+PIN_BTN_ROTATION_GAUCHE = Pin(12, Pin.IN, Pin.PULL_UP)
+PIN_BTN_ROTATION_DROITE = Pin(13, Pin.IN, Pin.PULL_UP)
 
-peer = b'0\xc6\xf70\x07\x98'   # MAC address of peer's wifi interface
-e.add_peer(peer)      # Must add_peer() before send()
+PIN_BTN_CANON_UP = Pin(27, Pin.IN, Pin.PULL_UP)
+PIN_BTN_CANON_DOWN = Pin(14, Pin.IN, Pin.PULL_UP)
+
+PIN_BTN_CANON_TIR = Pin(25, Pin.IN, Pin.PULL_UP)
+PIN_BTN_PLOT = Pin(26, Pin.IN, Pin.PULL_UP)
+
+
+
+
+COMMUNICATION = espnow.ESPNow()
+COMMUNICATION.active(True)
 
 centerX = (1965,1995)
 centerY = (1830,1855)
 
-host, msg = e.recv()
+wlan_sta = network.WLAN(network.STA_IF)
+wlan_sta.active(True)
+wlan_mac = wlan_sta.config('mac')
+print(wlan_mac)
+
+host, msg = COMMUNICATION.recv()
 if msg == b'PLEASE':  # msg == None if timeout in recv()
     print(host, msg)
     peer = host
-    e.send(peer, "CONNECTED", True)
+    COMMUNICATION.add_peer(peer)
+    COMMUNICATION.send(peer, "CONNECTED", True)
     
 while True:
-
     if not PIN_BTN_ACCESSOIRE.value():
-        print("btn appuyé")
+        print("btn sos")
+        COMMUNICATION.send(peer, "SOS", True)
+
+    if not PIN_INTERRUPT_GIRO2.value():
+        print("Giro On")
+
+    if not PIN_BTN_ROTATION_GAUCHE.value():
+        print("rotation gauche")
+        COMMUNICATION.send(peer, "RG", True)
+
+
+    if not PIN_BTN_ROTATION_DROITE.value():
+        print("rotation droite")
+        COMMUNICATION.send(peer, "RD", True)
+
+    if not PIN_BTN_CANON_UP.value():
+        print("canon up")
+        COMMUNICATION.send(peer, "UP", True)
+
+
+    if not PIN_BTN_CANON_DOWN.value():
+        print("Canon down")
+        COMMUNICATION.send(peer, "DOWN", True)
+
+    if not PIN_BTN_CANON_TIR.value():
+        print("tir")
+        COMMUNICATION.send(peer, "TIR", True)
+
+
+    if not PIN_BTN_PLOT.value():
+        print("plot")
+        COMMUNICATION.send(peer, "PLOT", True)
+    
 
     A = False
     R = False
@@ -49,7 +93,7 @@ while True:
     xValue = JOYSTICK_X.read()
     yValue = JOYSTICK_Y.read()
 
-    if(xValue > 1995):
+    if(xValue > centerX[1]):
         A = True
     if(yValue > 1855):
         D = True
@@ -60,23 +104,23 @@ while True:
 
     if A:
         if D:
-            e.send(peer, "DD", True)
+            COMMUNICATION.send(peer, "DD", True)
         elif G:
-            e.send(peer, "DG", True)
+            COMMUNICATION.send(peer, "DG", True)
         else:
-            e.send(peer, "A", True)
+            COMMUNICATION.send(peer, "A", True)
     elif R:
         if D:
-            e.send(peer, "DD", True)
+            COMMUNICATION.send(peer, "DBD", True)
         elif G:
-            e.send(peer, "DG", True)
+            COMMUNICATION.send(peer, "DBG", True)
         else:
-            e.send(peer, "R", True)
+            COMMUNICATION.send(peer, "R", True)
     elif D:
-        e.send(peer, "TD", True)
+        COMMUNICATION.send(peer, "TD", True)
     elif G:
-        e.send(peer, "TG", True)
+        COMMUNICATION.send(peer, "TG", True)
     else:
-        e.send(peer, "STOP", True)
+        COMMUNICATION.send(peer, "STOP", True)
 
     time.sleep(0.1)
