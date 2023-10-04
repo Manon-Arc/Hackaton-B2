b128a0148b673617d1af3b7ceca9d1ee5b6fce2b
ahliko <killian.guillemot@ynov.com>
2023-10-04 13:32:06 +0200

    Update Pin in canon.py

--- Canon/canon.py
+++ Canon/canon.py
@@ -1,10 +1,8 @@
 from machine import Pin
 from time import sleep
 
-motor1_step = Pin(17, Pin.OUT)
-motor1_dir = Pin(19, Pin.OUT)
-motor2_step = Pin(22, Pin.OUT)
-motor2_dir = Pin(23, Pin.OUT)
+step_pin = Pin(17, Pin.OUT)
+dir_pin = Pin(19, Pin.OUT)
 Canon = Pin(25, Pin.OUT)
 
 
@@ -14,16 +12,7 @@ def angle_to_steps(angle):
     return int(angle * 200 / 360)
 
 
-def move_motor(motor, steps, direction):
-    if motor == 1:
-        step_pin = motor1_step
-        dir_pin = motor1_dir
-    elif motor == 2:
-        step_pin = motor2_step
-        dir_pin = motor2_dir
-    else:
-        raise ValueError("Invalid motor number")
-
+def move_motor(steps, direction):
     dir_pin.value(direction)
     for i in range(steps):
         step_pin.value(1)
